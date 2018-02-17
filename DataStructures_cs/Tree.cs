941f438ae733d0787a706b9131cf28e63bed96d7
Osama Kawish <osamakawish@gmail.com>
2018-02-17 11:17:50 -0500

    Added Tree class.

---
+++ DataStructures_cs/Tree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_cs
+{
+    public class Tree
+    {
+        private Tree Parent { get; set; }
+        private HashSet<Tree> Children { get; set; }
+
+        public Tree()
+        {
+            Children = new HashSet<Tree>();
+        }
+
+        /// <summary>
+        /// Return true iff obj has the same root and children as this.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Tree))
+            {
+                return false;
+            }
+
+            obj = (Tree)obj;
+
+            // Want to ch
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
